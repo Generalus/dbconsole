eb3bbfc300f76076deb0f24e138ad5a26a240bba
P44elovod <eug.kovtun@gmail.com>
2017-10-15 22:22:14 +0300

    CommandInterface Test

--- src/test/java/com/thesn/dbconsole/ConfigurationTest.java
+++ src/test/java/com/thesn/dbconsole/ConfigurationTest.java
@@ -9,11 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ConfigurationTest {
-    String dbLocationControl = "--connection=jdbc:mysql://localhost:3306/my_database";
-    String passControl = "--password=pass";
-    String loginControl = "--login=login";
-    String queryControl = "--query=query";
-    String[] args = {dbLocationControl, passControl, loginControl,queryControl};
+    String expectedDbLocation = "--connection=jdbc:mysql://localhost:3306/my_database";
+    String expectedPassword = "--password=pass";
+    String expectedLogin = "--login=login";
+    String expectedQuery = "--query=query";
+    String[] args = {expectedDbLocation, expectedLogin, expectedPassword,expectedQuery};
 
     CommandInterface commandInterface = new CommandInterface(args);
 
@@ -24,26 +24,26 @@ class ConfigurationTest {
         assertAll("properties",
                 () -> {
 
-                    commandInterface.extractConfiguration().put(Argument.CONNECTION, dbLocationControl);
+                    commandInterface.extractConfiguration().put(Argument.CONNECTION, expectedDbLocation);
                     String dbLocation = commandInterface.extractConfiguration().get(Argument.CONNECTION);
-                    assertEquals(dbLocationControl,"--connection=" + dbLocation);
+                    assertEquals(expectedDbLocation,"--connection=" + dbLocation);
 
                 },
                 () -> {
-                    commandInterface.extractConfiguration().put(Argument.PASSWORD, passControl);
+                    commandInterface.extractConfiguration().put(Argument.PASSWORD, expectedPassword);
                     String pass = commandInterface.extractConfiguration().get(Argument.PASSWORD);
-                    assertEquals(passControl,"--password=" + pass);
+                    assertEquals(expectedPassword,"--password=" + pass);
 
                 },
                 () -> {
-                    commandInterface.extractConfiguration().put(Argument.LOGIN, loginControl);
+                    commandInterface.extractConfiguration().put(Argument.LOGIN, expectedLogin);
                     String login = commandInterface.extractConfiguration().get(Argument.LOGIN);
-                    assertEquals(loginControl,"--login=" + login);
+                    assertEquals(expectedLogin,"--login=" + login);
                 },
                 () -> {
-                    commandInterface.extractConfiguration().put(Argument.QUERY, queryControl);
+                    commandInterface.extractConfiguration().put(Argument.QUERY, expectedQuery);
                     String query = commandInterface.extractConfiguration().get(Argument.QUERY);
-                    assertEquals(queryControl, "--query=" + query);
+                    assertEquals(expectedQuery, "--query=" + query);
                 }
 
 
